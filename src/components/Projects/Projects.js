92232dc57759483ac3aaaf0eec120b6dca931575
Akshay Kumar Hiran <akshaykumarhiran2@gmail.com>
2024-05-17 19:29:44 +0530

    Fill deployment URL

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -72,7 +72,7 @@ function Projects() {
               title="Portfolio Website"
               description="My portfolio website showcases my comprehensive job profile and projects, highlighting my skills and achievements. It provides detailed insights into my professional journey, featuring examples of my work, and demonstrates my expertise and capabilities to potential employers and collaborators."
               ghLink="https://github.com/Ak3294/Akshay-Hiran-Portfolio"
-              demoLink=""
+              demoLink="https://akshayhiranportfolio.netlify.app/"
             />
           </Col>
 
